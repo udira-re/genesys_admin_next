0313daeff6d4ff8f77bc55e021cc897be15a3246
Amshu Pokharel <amshu.pokhrel@kotuko.it>
2025-08-19 11:33:41 +0545

    fix: in /3000 shown login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+// export default function Home() {
+//   return (
+//     <div className=" grid  items-center justify-items-center min-h-screen ">
+//       Hey Amshu
+//     </div>
+//   );
+// }
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className=" grid  items-center justify-items-center min-h-screen ">
-      Hey Amshu
-    </div>
-  );
+  // Redirect immediately to login page
+  redirect("/login");
+
+  // This return will never actually render
+  return null;
 }
