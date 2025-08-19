b1d96ad4ff58957144c5ffe952363917540ec17b
Amshu Pokharel <amshu.pokhrel@kotuko.it>
2025-08-19 12:24:45 +0545

    fix: fixed typesscript new

--- app/(main)/users/[id]/page.tsx
+++ app/(main)/users/[id]/page.tsx
@@ -1,15 +1,10 @@
-import React from "react";
 import UserDetailClient from "@/components/skeleton/userDetailClient";
 
-// Next.js App Router expects params as Record<string, string>
-type PageProps = {
-  params: Record<string, string>;
-};
+interface UserDetailPageProps {
+  params: Promise<{ id: string }>;
+}
 
-const UserDetailPage: React.FC<PageProps> = ({ params }) => {
-  const { id } = params;
-
+export default async function UserDetailPage({ params }: UserDetailPageProps) {
+  const { id } = await params; // await the Promise
   return <UserDetailClient id={id} />;
-};
-
-export default UserDetailPage;
+}
